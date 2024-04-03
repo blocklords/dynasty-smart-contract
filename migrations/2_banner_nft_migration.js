491d257177cc89dcdec6c86bf78da5651152fde2
seasonlei <30384084@qq.com>
2024-04-03 18:56:18 +0800

    update deployment scripts

--- migrations/2_banner_nft_migration.js
+++ migrations/2_banner_nft_migration.js
@@ -1,10 +1,11 @@
 var Nft = artifacts.require("./BannerNFT.sol");
 
 const owner = "0xC6EF8A96F20d50E347eD9a1C84142D02b1EFedc0";
+const verifier = "0x5bDed8f6BdAE766C361EDaE25c5DC966BCaF8f43";
 
 module.exports = function(deployer, network, accounts) {
    
-    deployer.deploy(Nft, owner).then(function(){
+    deployer.deploy(Nft, owner, verifier).then(function(){
         console.log(`${accounts[0]} deployed Banner Nft on ${Nft.address}`);
     });
 };
--- migrations/3_house_nft_migration.js
+++ migrations/3_house_nft_migration.js
@@ -2,10 +2,11 @@ var Nft = artifacts.require("./HouseNFT.sol");
 
 const owner = "0xC6EF8A96F20d50E347eD9a1C84142D02b1EFedc0";
 const verifier = "0x5bDed8f6BdAE766C361EDaE25c5DC966BCaF8f43";
+const heroNft = "0x6401EBC94366fD9fdA6145f196f6172F2DC0AA26";
 
 module.exports = function(deployer, network, accounts) {
    
-    deployer.deploy(Nft, owner, verifier).then(function(){
+    deployer.deploy(Nft, owner, heroNft, verifier).then(function(){
         console.log(`${accounts[0]} deployed House Nft on ${Nft.address}`);
     });
 };
