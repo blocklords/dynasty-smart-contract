9374ce6e6fe2559445fa67e8ab13f5d6b00c707c
seasonlei <30384084@qq.com>
2024-05-10 15:09:02 +0800

    Fixed a bug where house could not be mint and added comments

--- contracts/nfts/HouseNFT.sol
+++ contracts/nfts/HouseNFT.sol
@@ -6,18 +6,19 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 /**
- *  @title BLOCKLORDS
- *  @author BLOCKLORDS TEAM
- *  @notice The BLHS token
+ * @title BLOCKLORDS
+ * @dev A contract for managing House NFTs
+ * @author BLOCKLORDS TEAM
+ * @notice This contract facilitates the creation and management of House NFTs
  */
  
 contract HouseNFT is ERC721Enumerable, Ownable {
     
-    bool    private lock;
-    uint256 private nextTokenId;
-    string  private baseUri;
-    address public  verifier;
-    address public  heroNft;
+    bool    private lock;                      // Reentrancy guard
+    uint256 private nextTokenId;               // Counter for generating the next token ID
+    string  private baseUri;                   // Base URI for token metadata
+    address public  verifier;                  // Address of the verifier contract for signature verification
+    address public  heroNft;                   // Address of the Hero NFT contract
 
     struct HouseParams {
         uint256 flagShape;
@@ -27,16 +28,22 @@ contract HouseNFT is ERC721Enumerable, Ownable {
         uint256 lordNftId;
     }
 
-    mapping(uint256 => bool) private soulbound;
-    mapping(uint256 => HouseParams) public houseParams;
-    mapping(address => uint256) public nonce;
-    mapping(address => bool) public exists;
+    mapping(uint256 => bool) private soulbound;         // Mapping to track if a token is soulbound
+    mapping(uint256 => HouseParams) public houseParams; // Mapping to store house parameters
+    mapping(address => uint256) public nonce;           // Nonce for signature verification
+    mapping(address => bool) public exists;             // Mapping to track if an address owns a house NFT
 
     event Minted(address indexed to, uint256 indexed tokenId, uint256 indexed time);
     event SetHouse(address indexed player, uint256 indexed houseId, uint256 indexed lordNftId, uint256 time);
     event SetVerifier(address indexed verifier, uint256 indexed time);
     event SetHeroNft(address indexed heroNftAddress, uint256 indexed time);
 
+    /**
+     * @dev Initializes the contract with the provided addresses.
+     * @param initialOwner The address that will become the owner of the contract.
+     * @param _heroNft Address of the Hero NFT contract.
+     * @param _verifier Address of the verifier for signature verification.
+     */
     constructor(address initialOwner, address _heroNft, address _verifier) ERC721("Blocklords House", "BLHS") Ownable(initialOwner) {
         require(_verifier != address(0), "verifier can't be zero address");
         require(_heroNft != address(0), "hero nft address not zero");
@@ -46,6 +53,9 @@ contract HouseNFT is ERC721Enumerable, Ownable {
         heroNft = _heroNft;
     }
 
+    /**
+     * @dev Modifier to prevent reentrancy attacks.
+     */
     modifier nonReentrant() {
         require(!lock, "no reentrant call");
         lock = true;
@@ -53,18 +63,28 @@ contract HouseNFT is ERC721Enumerable, Ownable {
         lock = false;
     }
 
-    function safeMint(address _to, bytes calldata _data, uint8 _v, bytes32 _r, bytes32 _s) external nonReentrant returns(uint256) {
+    /**
+     * @dev Mints a new House NFT.
+     * @param _to Address to mint the token to.
+     * @param _data Encoded data containing the parameters of the house.
+     * @param _v ECDSA signature parameter v.
+     * @param _r ECDSA signature parameter r.
+     * @param _s ECDSA signature parameter s.
+     * @return The ID of the minted token.
+     */
+    function safeMint(address _to, bytes calldata _data, uint256 _deadline, uint8 _v, bytes32 _r, bytes32 _s) external nonReentrant returns(uint256) {
         require(!exists[_to], "you already own the house nft");
 
-        (uint256 flagShape, uint256 houseSymbol, uint256 flagColor, string memory houseName, uint256 deadline) 
-            = abi.decode(_data, (uint256, uint256, uint256, string, uint256));
+        (uint256 flagShape, uint256 houseSymbol, uint256 flagColor, string memory houseName) 
+            = abi.decode(_data, (uint256, uint256, uint256, string));
 
-        require(deadline >= block.timestamp, "signature has expired");
+        require(_deadline >= block.timestamp, "signature has expired");
         require(flagShape > 0 && flagColor > 0 && houseSymbol > 0 &&  bytes(houseName).length > 0, "house params err");
 
         uint256 tokenId = nextTokenId++;
       
-        verifySignature(_to, _data, _v, _r, _s);
+        // Verify the signature
+        verifySignature(_to, _data, _deadline, _v, _r, _s);
         
         exists[_to]                = true;
         
@@ -75,25 +95,34 @@ contract HouseNFT is ERC721Enumerable, Ownable {
         house.houseName           = houseName;
         house.lordNftId           = 0;
 
-        soulbound[tokenId]        = true;
         nonce[_to]++;
 
         _safeMint(_to, tokenId);
+
+        soulbound[tokenId]        = true;
 
         emit Minted(_to, tokenId, block.timestamp);
         return tokenId;
     }
 
-    function setHouse (address _from, bytes calldata _data, uint8 _v, bytes32 _r, bytes32 _s) external nonReentrant{
-        (uint256 flagShape, uint256 houseSymbol, uint256 flagColor, string memory houseName, uint256 lordNftId, uint256 houseId, uint256 deadline) 
-            = abi.decode(_data, (uint256, uint256, uint256, string, uint256, uint256, uint256));
+    /**
+     * @dev Sets the parameters of a House NFT.
+     * @param _from Address of the token owner.
+     * @param _data Encoded data containing the parameters of the house.
+     * @param _v ECDSA signature parameter v.
+     * @param _r ECDSA signature parameter r.
+     * @param _s ECDSA signature parameter s.
+     */
+    function setHouse(address _from, bytes calldata _data, uint256 _deadline, uint8 _v, bytes32 _r, bytes32 _s) external nonReentrant{
+        (uint256 flagShape, uint256 houseSymbol, uint256 flagColor, string memory houseName, uint256 lordNftId, uint256 houseId) 
+            = abi.decode(_data, (uint256, uint256, uint256, string, uint256, uint256));
 
-        require(deadline >= block.timestamp, "signature has expired");
+        require(_deadline >= block.timestamp, "signature has expired");
         require(IERC721(address(this)).ownerOf(houseId) == _from, "not house nft owner");
-        // require(IERC721(heroNft).ownerOf(lordNftId) == _from, "not hero nft owner");
         require(flagShape > 0 && flagColor > 0 && houseSymbol > 0 &&  bytes(houseName).length > 0, "house params err");
 
-        verifySignature(_from, _data, _v, _r, _s);
+        // Verify the signature
+        verifySignature(_from, _data, _deadline, _v, _r, _s);
         
         HouseParams storage house = houseParams[houseId];
         house.flagShape           = flagShape;
@@ -104,28 +133,39 @@ contract HouseNFT is ERC721Enumerable, Ownable {
         
         nonce[_from]++;
         emit SetHouse(_from, houseId, lordNftId, block.timestamp);
-
     }
 
-    // Verifying vrs
-    function verifySignature(address _addr, bytes calldata _data, uint8 _v, bytes32 _r, bytes32 _s) internal view {
+    /**
+     * @dev Verifies the signature of a transaction.
+     * @param _addr The address of the signer.
+     * @param _data The data being signed.
+     * @param _deadline The expiry timestamp for the signature.
+     * @param _v The ECDSA signature parameter v.
+     * @param _r The ECDSA signature parameter r.
+     * @param _s The ECDSA signature parameter s.
+     */
+    function verifySignature(address _addr, bytes calldata _data, uint256 _deadline, uint8 _v, bytes32 _r, bytes32 _s) internal view {
         bytes memory prefix     = "\x19Ethereum Signed Message:\n32";
-        bytes32 message         = keccak256(abi.encodePacked(nonce[_addr], _addr, _data, address(this), block.chainid));
+        bytes32 message         = keccak256(abi.encodePacked(nonce[_addr], _addr, _data, _deadline, address(this), block.chainid));
         bytes32 hash            = keccak256(abi.encodePacked(prefix, message));
         address recover = ecrecover(hash, _v, _r, _s);
         require(recover == verifier, "verification failed");
     }
 
     // Method called by the contract owner
-    function setBaseURI(string calldata _baseUri) external onlyOwner() {
-        baseUri = _baseUri;
-    }
-
+    /**
+     * @dev Sets the address of the verifier contract for signature verification.
+     * @param _verifier The address of the new verifier contract.
+     */
     function setVerifier (address _verifier) external onlyOwner {
         require(_verifier != address(0), "verifier can't be zero address ");
         verifier = _verifier;
 
         emit SetVerifier(_verifier, block.timestamp);
+    }
+
+    function setBaseURI(string calldata _baseUri) external onlyOwner() {
+        baseUri = _baseUri;
     }
 
     function setHeroNft (address _nft) external onlyOwner {
@@ -152,7 +192,6 @@ contract HouseNFT is ERC721Enumerable, Ownable {
     function _baseURI() internal view override returns (string memory) {
         return baseUri;
     }
-
 }
 
 
